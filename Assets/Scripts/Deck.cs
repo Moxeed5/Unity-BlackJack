35fa709a2789b98fbf0431dad7f87654336320c1
Max Morgan <maxmmorgan93@gmail.com>
2024-01-16 16:14:35 -0600

    Apply .gitignore and clean up repository

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+
+    public Sprite[] cardSprites;
+    int[] cardValues = new int[53];
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

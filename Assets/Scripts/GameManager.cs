585d50669bb3c387f6ee10c407fd9aef2bfeed2d
Max Morgan <maxmmorgan93@gmail.com>
2024-01-19 23:28:45 -0600

    completed DealClicked func

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,28 @@ public class GameManager : MonoBehaviour
     public Button hitBtn;
     public Button standBtn;
     public Button betBtn;
+    
+    
+
+    private int standClicks = 0;
 
     //Access the player and dealer's script
     public PlayerScript playerScript;
     public PlayerScript dealerScript;
+
+    //public text to acccess and update the hud
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI dealerScoreText;
+    public TextMeshProUGUI betsText;
+    public TextMeshProUGUI cashText;
+    //public Text mainText;
+    public TextMeshProUGUI standBtnText;
+
+    //Card hiding dealers 2nd Card
+    public GameObject hideCard;
+    int pot = 0;
+       
+
 
     void Start()
     {
@@ -27,19 +46,49 @@ public class GameManager : MonoBehaviour
 
     private void DealClicked()
     {
+        //hides dealers card at the start of the deal
+        //commenting out the below fixed deal button not working
+        dealerScoreText.gameObject.SetActive(false);
         GameObject.Find("Deck").GetComponent<Deck>().Shuffle();
         playerScript.StartHand();
         dealerScript.StartHand();
+        //insert the sum of player's hand and dealer's hand into 
+        scoreText.text = "Hand: " + playerScript.handValue.ToString();
+        dealerScoreText.text = "Hand: " + playerScript.handValue.ToString();
+        //adjust buttons visibility 
+        dealBtn.gameObject.SetActive(false);
+        hitBtn.gameObject.SetActive(true);
+        standBtn.gameObject.SetActive(true);
+        TextMeshProUGUI buttonText = standBtn.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = "Stand";
+        pot = 40;
+        betsText.text = pot.ToString();
+
     }
 
     private void HitClicked()
     {
-        throw new NotImplementedException();
+        if(playerScript.GetCard() <= 10)
+        {
+            playerScript.GetCard();
+        }
     }
 
     private void StandClicked()
     {
-        throw new NotImplementedException();
+        standClicks++;
+        if (standClicks > 1) Debug.Log("end function");
+        HitDealer();
+        standBtnText.text = "Call";
+
+    }
+
+    private void HitDealer()
+    {
+        while (dealerScript.handValue < 16 && dealerScript.cardIndex < 10)
+        {
+            dealerScript.GetCard();
+        }
     }
 
 }
